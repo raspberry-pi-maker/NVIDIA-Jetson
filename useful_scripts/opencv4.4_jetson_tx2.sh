76a9f473da166762dc3be439cf911c75f38fed40
raspberry-pi-maker <raspberry.pi.maker@gmail.com>
2020-08-26 22:25:49 +0900

    update

--- useful_scripts/opencv4.4_jetson_tx2.sh
+++ useful_scripts/opencv4.4_jetson_tx2.sh
@@ -42,7 +42,7 @@ echo "** Building..."
 mkdir release
 cd release/
 cmake -D WITH_CUDA=ON -D ENABLE_PRECOMPILED_HEADERS=OFF  -D CUDA_ARCH_BIN="6.2" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.4.0/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j6
+make -j4
 sudo make install
 
 echo "** Install opencv-4.4.0 successfully"
